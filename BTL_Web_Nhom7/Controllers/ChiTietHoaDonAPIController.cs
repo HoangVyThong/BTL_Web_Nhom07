10a0dc357b705ab4d4e06433f8891599e7f1ca7d
minhlephu <110329077+minhlephu@users.noreply.github.com>
2023-04-12 16:48:30 +0700

    hi

---
+++ BTL_Web_Nhom7/Controllers/ChiTietHoaDonAPIController.cs
@@ -0,0 +1,31 @@
+﻿using BTL_Web_Nhom7.Models;
+using BTL_Web_Nhom7.Models.HoaDonModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_Web_Nhom7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChiTietHoaDonAPIController : ControllerBase
+    {
+        BtlWebContext db = new BtlWebContext();
+        [Route("{SoHD}")]
+        public IEnumerable<ChiTietHoaDonDTO> GetChiTietHoaDon(string SoHD)
+        {
+            var hoadon = (from p in db.ChiTietHdbs
+                     join q in db.ThietBiYtes on p.MaThietBi equals q.MaThietBi
+                     where p.SoHdb == SoHD
+                     select new ChiTietHoaDonDTO
+                     {
+                         SoHdb = p.SoHdb,
+                         TenThietBi = q.TenThietBi,
+                         Anh = q.Anh,
+                         SoLuong = q.SoLuong,
+                         GiaBan = q.GiaBan,
+                         ThanhTien = p.ThanhTien
+                     }).ToList();
+            return hoadon;
+        }
+    }
+}
